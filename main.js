5828844e32758ebe2bcdad8a6919e263e91e1063
Eric Masi <eric@gitmasi.com>
2019-02-23 15:08:34 -0500

    add logic to panel 4

--- main.js
+++ main.js
@@ -33,23 +33,19 @@ sortButton.addEventListener('click', ()=> {
 
 // panel 3 guess 
 
-const guessForm = document.querySelector('#panel-3 > section > div.panel-display > div > form');
-const guessNumberInput = document.querySelector('#panel-3 > section > div.panel-display > div > form > input[type="number"]');
+const guessForm = document.querySelector('#panel-3 > section > div.panel-display > div > div');
+const guessNumberInput = document.querySelector('#panel-3 > section > div.panel-display > div > div > input[type="number"]');
 const tellMeButton = document.querySelector('#panel-3 > section > div.panel-buttons > button');
 const submitGuessButton = document.querySelector('#panel-3 > section > div.panel-buttons > a');
 let guessAmount = 0;
-
-if(guessForm.addEventListener){
-  guessForm.addEventListener('submit', ()=>{
-    guessAmount = Number(guessNumberInput.value);
-  }, false);
-}
 
 guessNumberInput.addEventListener('keypress', (e)=> {
   let key = e.which || e.keyCode;
   if (key === 13) {
     guessAmount = Number(guessNumberInput.value);
+    submitGuessButton.click();
   }
+
 });
 
 submitGuessButton.addEventListener('click', ()=> {
@@ -68,4 +64,31 @@ function palindromeCheck(arr){
   return arr.filter(word => word == word.split('').reverse().join(''));
 }
 
+const panel4 = document.querySelector('#panel-4');
+const guessStats = document.querySelector('#panel-4 > section > div.panel-display > div.display-wrapper.p-2 > p');
 const finalPalindromeList = palindromeCheck(wordsMasterArr);
+const palindromeTotal = finalPalindromeList.length;
+const palindromeListDisplay = document.querySelector('#palindrome-list');
+
+let checkGuess = ()=>{
+  let num = Math.abs(Number(guessAmount - palindromeTotal)).toString();
+  switch (true) {
+    case (num == 0):
+      return `Amazing, ${palindromeTotal} is right on!`;
+    case (num < palindromeTotal):
+      return `Wow, that's pretty close! You were only off by ${num}, the actual number is ${palindromeTotal}.`
+    case (num > palindromeTotal && num < palindromeTotal * 2):
+      return `Wow, that's pretty close! You were only off by ${num}, the actual number is ${palindromeTotal}.`
+    case (num >= palindromeTotal * 2):
+      return `Good guess, though you were off by ${num}. The actual number of palindromes is ${palindromeTotal}.`
+  }
+};
+
+window.addEventListener('scroll', () => {
+  if (panel4.getBoundingClientRect().top == 0){
+    guessStats.textContent = `${checkGuess()}`
+    finalPalindromeList.forEach(word => palindromeListDisplay.textContent += word + '\n');
+  } else {
+    palindromeListDisplay.textContent = '';
+  };
+});
